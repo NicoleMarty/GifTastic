98c17aa8b464259849fe29710e2b9cbc1cda8f1b
Nicole Marty <nmarty310@gmail.com>
2019-08-06 16:55:22 -0500

    not sure if this is some sort of joke but I get gifs for indefinate loops when I try to add a new button lol

--- javascript.js
+++ javascript.js
@@ -1,8 +1,8 @@
 // Array of things
 var hipsterThings = ["beards", "vintage", "pabst-blue-ribbon", "indie-rock", "unicycle", "gentrification"];
-// Generic function for capturing the movie name from the data-attribute
+// Generic function for capturing the new hipster thing from the data-attribute
 function alertNewHipsterThing() {
-    var newHipsterThing = $(this).attr("data-name");
+    var newHipsterThing = $(this).attr("data-hipsterThings");
 
     alert(newHipsterThing);
 }
@@ -33,9 +33,29 @@ $("button").on("click", function() {
         };
     });
 });
-
 $("#add-hipsterThing").on("click", function(event) {
     event.preventDefault();
-    var newHipsterThing = $("#hipster-input").val().trim();
-    alert(newHipsterThing);
+    var newHipsterThing = $(this).attr("data-newHipsterThing");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+        newHipsterThing + "&api_key=lunA3lE4OLv8yrxJ6Ppj3KF3bl4ZjFq9&limit=10";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        console.log(response);
+        var results = response.data;
+
+        for (var i = 0; i < results.length; i++) {
+            var newHipsterThingDiv = $("<div>");
+            var p = $("<p>");
+            var rating = results[i].rating;
+            var p = $("<p>").text("Rating: " + rating);
+            var newHipsterThingImage = $("<img>");
+            newHipsterThingImage.attr("src", results[i].images.fixed_height.url);
+            newHipsterThingDiv.append(p);
+            newHipsterThingDiv.append(newHipsterThingImage);
+            $("#gifs-appear-here").prepend(newHipsterThingDiv);
+        };
+    });
 });
