19ad64b8905627787a197fa1736780eb81a83456
Nicole Marty <nmarty310@gmail.com>
2019-08-07 16:04:32 -0500

    working on getting the pause/play (animate/still) and clearing out gifs upon entering a new thing. Need to get the new thing to add as a button and show gifs.

--- javascript.js
+++ javascript.js
@@ -1,7 +1,9 @@
 // Array of things
 var hipsterThings = ["beards", "vintage", "pabst-blue-ribbon", "indie-rock", "unicycle", "gentrification"];
 // Generic function for capturing the new hipster thing from the data-attribute
-function alertNewHipsterThing() {
+function NewHipsterThing(hipsterThings, hipsterThingsDiv) {
+    $(hipsterThingsDiv).empty(); // Empty the hipsterThingsDiv
+
     var newHipsterThing = $(this).attr("data-hipsterThings");
 
     alert(newHipsterThing);
@@ -25,37 +27,12 @@ $("button").on("click", function() {
             var p = $("<p>");
             var rating = results[i].rating;
             var p = $("<p>").text("Rating: " + rating);
+            var animated = response.data[i].images.fixed_height.url;
+            var still = response.data[i].images.fixed_height_still.url;
             var hipsterThingsImage = $("<img>");
-            hipsterThingsImage.attr("src", results[i].images.fixed_height.url);
             hipsterThingsDiv.append(p);
             hipsterThingsDiv.append(hipsterThingsImage);
             $("#gifs-appear-here").prepend(hipsterThingsDiv);
-        };
-    });
-});
-$("#add-hipsterThing").on("click", function(event) {
-    event.preventDefault();
-    var newHipsterThing = $(this).attr("data-newHipsterThing");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-        newHipsterThing + "&api_key=lunA3lE4OLv8yrxJ6Ppj3KF3bl4ZjFq9&limit=10";
-
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function(response) {
-        console.log(response);
-        var results = response.data;
-
-        for (var i = 0; i < results.length; i++) {
-            var newHipsterThingDiv = $("<div>");
-            var p = $("<p>");
-            var rating = results[i].rating;
-            var p = $("<p>").text("Rating: " + rating);
-            var newHipsterThingImage = $("<img>");
-            newHipsterThingImage.attr("src", results[i].images.fixed_height.url);
-            newHipsterThingDiv.append(p);
-            newHipsterThingDiv.append(newHipsterThingImage);
-            $("#gifs-appear-here").prepend(newHipsterThingDiv);
         };
     });
 });
